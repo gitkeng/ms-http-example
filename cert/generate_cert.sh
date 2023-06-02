27e1c51e5f31fd379708dd2dde5c223138f8de00
Kongsak <keng.lim@outlook.com>
2023-06-02 08:07:50 +0700

    [feature] add build configuration

--- cert/generate_cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE_CERT_NAME=localhost
-openssl req -x509 \
-            -sha512 -days 18250 \
-            -nodes \
-            -newkey rsa:4096 \
-            -subj "/CN=selfsigned.app/C=TH/L=Bangkok" \
-            -addext "subjectAltName = DNS:selfsigned.app" \
-            -keyout "$FILE_CERT_NAME.key" -out "$FILE_CERT_NAME.crt"
